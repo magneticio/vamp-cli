0d60ff7e0cf47b6fa2638b6462e0246682f4649a
Tim Nolet <tim@magnetic.io>
2017-09-15 12:08:59 +0200

    removes unused stdin on deployment

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -3,16 +3,11 @@ const handleError = require('../logging').handleError
 module.exports = (program) => {
   program
     .command('deploy <blueprint> <deployment>')
-    .option('--stdin [file]', 'read from std in.')
     .description('Creates a deployment, based on a blueprint, with a specified name.')
     .action((blueprint, deployment, options) => {
-      if (options.stdin) {
-        console.log(options.parent.rawArgs.reverse()[0])
-      } else {
-        api.deployment.deploy(deployment, blueprint)
-          .then(handleResult)
-          .catch(handleError)
-      }
+      api.deployment.deploy(deployment, blueprint)
+        .then(handleResult)
+        .catch(handleError)
     })
 }
 
